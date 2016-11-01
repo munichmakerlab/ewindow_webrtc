8b3cd782f1084810ef45d8655fd0b28dd2e1f9c1
Jürgen Skrotzky <JorgenVikingGod@gmail.com>
2016-11-01 22:17:20 +0100

    add "template" instead of add html as string

--- static/js/admin.js
+++ static/js/admin.js
@@ -17,15 +17,12 @@ function loadUsers() {
     headers: {"Authorization": "Bearer " + token},
     success: function(data) {
       data.forEach(function(user) {
-        $("#userlist").append("<tr>" +
-                              "  <td><input type=\"checkbox\" class=\"checkthis\" /></td>" +
-                              "  <td>" + user.name + "</td>" +
-                              "  <td>" + user.ip + "</td>" +
-                              "  <td>" + user.last_login + "</td>" + 
-                              "  <td><p data-placement=\"top\" data-toggle=\"tooltip\" title=\"Edit\"><button class=\"btn btn-primary btn-xs\" data-title=\"Edit\" data-toggle=\"modal\" data-target=\"#edit\" ><span class=\"glyphicon glyphicon-pencil\"></span></button></p></td>" +
-                              "  <td><p data-placement=\"top\" data-toggle=\"tooltip\" title=\"Delete\"><button class=\"btn btn-danger btn-xs\" data-title=\"Delete\" data-toggle=\"modal\" data-target=\"#delete\" ><span class=\"glyphicon glyphicon-trash\"></span></button></p></td" +
-                              "</tr>"
-                            );
+        var template = $('#hidden-template').html()
+                        .replace('{USER_ACTIVE}', user.active ? 'checked':'')
+                        .replace('{USER_NAME}', user.name)
+                        .replace('{USER_IP}', user.ip)
+                        .replace('{USER_LAST_LOGIN}', user.last_login);
+        $("#userlist").append(template);
       });
     }
   });
